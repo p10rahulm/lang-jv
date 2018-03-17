f6070c513ef615cbc1c0d5150aa4c27a24bf41e9
Rahul Madhavan <rahul.maddy@gmail.com>
2018-03-18 00:36:23 +0530

    elements

---
+++ Sedgewick/UseArgumentHello.java
@@ -0,0 +1,9 @@
+
+public class UseArgumentHello {
+    public static void main(String[] args){
+        System.out.print("Hi, ");
+        System.out.print(args[0]);
+        System.out.print(". How are you?");
+        System.out.println();
+    }
+}
