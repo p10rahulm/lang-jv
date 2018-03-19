3ddb6cf6f022c87ec498167e25dbcd1a656c0301
Rahul Madhavan <rahul.maddy@gmail.com>
2018-03-19 20:03:26 +0530

    elements

--- Sedgewick/Quadratic.java
+++ Sedgewick/Quadratic.java
@@ -3,11 +3,16 @@ public class Quadratic {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-        double discriminant = b*b - 4*a*c;
-        double root1 = (-b + Math.pow(discriminant,0.5))/(2*a);
-        double root2 = (-b - Math.pow(discriminant,0.5))/(2*a);
-        System.out.println("root1 = "+root1);
-        System.out.println("root2 = "+root2);
+        if(a==0){
+            System.out.println("root1 = "+b/c);
+        }
+        else {
+            double discriminant = b * b - 4 * a * c;
+            double root1 = (-b + Math.pow(discriminant, 0.5)) / (2 * a);
+            double root2 = (-b - Math.pow(discriminant, 0.5)) / (2 * a);
+            System.out.println("root1 = " + root1);
+            System.out.println("root2 = " + root2);
+        }
 
     }
 }
---
+++ Sedgewick/numHellos.java
@@ -0,0 +1,17 @@
+public class numHellos {
+    public static void main(String[] args){
+        int num = Integer.parseInt(args[0]);
+        for(int i =1;i<=num;i++){
+            String helloi = "This is the ";
+            if((i%100)/10!=1){
+                if(i%10 == 1){helloi = helloi + i + "st Hello.";}
+                else if(i%10 == 2){helloi = helloi + i + "nd Hello.";}
+                else if(i%10 == 3){helloi = helloi + i + "rd Hello.";}
+                else {helloi = helloi + i + "th Hello.";}
+            } else{
+                helloi = helloi + i + "th Hello.";
+            }
+            System.out.println(helloi);
+        }
+    }
+}
