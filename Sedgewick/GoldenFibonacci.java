b147337d594a386371330d923e074709a68686c0
Rahul Madhavan <rahul.maddy@gmail.com>
2018-03-19 20:28:27 +0530

    elements

---
+++ Sedgewick/GoldenFibonacci.java
@@ -0,0 +1,13 @@
+public class GoldenFibonacci {
+    public static void main(String[] args){
+        int num =Integer.parseInt(args[0]);
+        int temp,f=0, g=1;
+        double golden=1.0;
+        for(int i =0;i<=num;i++){
+            System.out.println(f);
+            temp = f;f=f+g;g=temp;
+            if(g>1){golden = (double) f/g;}
+            System.out.println("Golden Ratio Estimate: " + golden);
+        }
+    }
+}
