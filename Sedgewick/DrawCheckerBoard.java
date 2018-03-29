38d7e8197687f303ee6b5a826bf2329d1902417d
Rahul Madhavan <rahul.maddy@gmail.com>
2018-03-29 19:00:24 +0530

    Elements

---
+++ Sedgewick/DrawCheckerBoard.java
@@ -0,0 +1,12 @@
+public class DrawCheckerBoard {
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        StdOut.println(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((i+j)%2==0){StdDraw.setPenColor(StdDraw.RED);} else{StdDraw.setPenColor(StdDraw.BLACK);}
+                StdDraw.filledSquare((double)(2*i+1)/(2*n),(double)(2*j+1)/(2*n),(double)1/(n*2));
+            }
+        }
+    }
+}
